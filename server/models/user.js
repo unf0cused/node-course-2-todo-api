782560601adc39e24b12696a6edd8b8ccd00926b
Unfocused <unfocused@wp.pl>
2017-11-07 23:47:29 +0100

    Add GET /todos route

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,12 @@
 var mongoose = require('mongoose');
 
-var User = mongoose.model('User',
-{
-    email:
-    {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-module.exports = {User};
+module.exports = {User}
